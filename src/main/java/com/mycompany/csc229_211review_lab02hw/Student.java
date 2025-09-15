f558ade7a0985f1a2d763e9dca5c0dfd5d17d738
ashrs98-collab <ashrs98@farmingdale.edu>
2025-09-14 20:23:02 -0400

    Update Student.java

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,37 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+public class Student extends Person {
+    private String address; // to extend parent class method
+    private int gpa;  // to set new child class attribute
+
+    Student(int gpa, String address, String name, short age){
+        super(name,age);
+        this.gpa = gpa;
+        this.address = address;
+    } // Student class with all fields from parent and student
+
+    public void setGpa(int gpa){
+        this.gpa = gpa;
+    }
+    public int getGpa(){
+        return this.gpa;
+    }
+
+    public String toString(){
+        return "name: " + getName() + "Gpa: " + gpa + "address: "+ address + "Age "+ getAge();
+
+    }
+
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }                                                  // Override methods from parent class
+    @Override
+    public String getAddress(){
+        return this.address;
+    }
+
 
 }
